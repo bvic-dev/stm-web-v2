2bccdeb3c7136abc4e0a2e8211eef72b304cd61b
Victorien <b.vic.dev@gmail.com>
2025-06-12 12:07:41 +0200

    💄 Add Footer

---
+++ src/data/navLinks.ts
@@ -0,0 +1,12 @@
+export const headerNavLinks = [
+  { href: "/", label: "Sport Track Merger" },
+  { href: "/blog", label: "Blog" },
+  { href: "/faq", label: "FAQ" },
+  { href: "/changelog", label: "Changelog" },
+  { href: "/contact", label: "Contact" },
+];
+
+export const footerNavLinks = [
+  { href: "/privacy", label: "Privacy" },
+  ...headerNavLinks.slice(1),
+];
