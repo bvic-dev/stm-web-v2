e9b9cb6a3a397bbedde39c18329fbd6f3da9bd38
Victorien <victoto56@gmail.com>
2025-06-16 17:29:24 +0200

    🐛 : Add Home route

--- src/utils/navLinks.ts
+++ src/utils/navLinks.ts
@@ -3,17 +3,18 @@ import FacebookLogo from "../assets/facebook.svg";
 import InstagramLogo from "../assets/instagram.svg";
 import DiscordLogo from "../assets/discord.svg";
 
-export const headerNavRoutes = [
+export const allNavRoutes = [
+  { key: "home", href: "/" },
   { key: "blog", href: "blog" },
   { key: "faq", href: "faq" },
   { key: "changelog", href: "changelog" },
   { key: "contact", href: "contact" },
+  { key: "privacy", href: "privacy" },
 ];
 
-export const footerNavRoutes = [
-  { key: "privacy", href: "privacy" },
-  ...headerNavRoutes.slice(1),
-];
+export const headerNavRoutes = allNavRoutes.filter(route => route.key !== "home" && route.key !== "privacy");
+
+export const footerNavRoutes = allNavRoutes.filter(route => route.key !== "home");
 
 export const socialNavRoutes = [
   { key: "strava", href: "https://www.strava.com/clubs/sporttrackmerger", icon: StravaLogo },
